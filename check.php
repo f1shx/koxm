9559ded5e8010c262aff91c1a077bea9901eff44
f1shx <97771248+f1shx@users.noreply.github.com>
2022-01-16 06:52:28 +0700

    Update check.php

--- check.php
+++ check.php
@@ -1,6 +1,6 @@
 <html>
 <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta http-equiv="REFRESH" content="0;https://chat.whatsapp.com/DAUUVI4R1Oy3AtjmDweNnN">
+<meta http-equiv="REFRESH" content="0;https://chat.whatsapp.com/IKWd2eYa9Si35eIAuYmbWj">
 </head>
 <body>
 </body>
