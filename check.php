e686f0a84e9abecc835c21f50ba728e4087221d4
f1shx <97771248+f1shx@users.noreply.github.com>
2022-01-16 06:25:10 +0700

    Add files via upload

---
+++ check.php
@@ -0,0 +1,7 @@
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="REFRESH" content="0;https://chat.whatsapp.com/DAUUVI4R1Oy3AtjmDweNnN">
+</head>
+<body>
+</body>
+</html>
